af0bb58f73651b535816ae552732ee216ecd6985
returntears <returntears@outlook.com>
2022-02-08 23:30:07 +0800

    async await

--- src/assets/js/asynch.await.js
+++ src/assets/js/asynch.await.js
@@ -3,6 +3,10 @@ import thenFs from "then-fs";
 async function getAllFile() {
     const r1 = await thenFs.readFile('../files/1.txt', 'utf8')
     console.log(r1)
+    const r2 = await thenFs.readFile('../files/2.txt', 'utf8')
+    console.log(r2)
+    const r3 = await thenFs.readFile('../files/3.txt', 'utf8')
+    console.log(r3)
 }
 
 getAllFile()
